f84e0a3814cf8cc4ff7b23b9474b4b1f6e563a73
ksheer506 <ksheer506@naver.com>
2024-10-15 09:41:28 +0900

    chore: eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@stylistic": stylistic,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "array-callback-return": "error",
+      "no-console": "warn",
+      "prefer-arrow-callback": "error",
+      "func-call-spacing": ["error", "never"],
+      "@stylistic/block-spacing": "error",
+      "@stylistic/semi": ["error", "never"],
+      "@stylistic/comma-dangle": [
+        "error",
+        {
+          arrays: "never",
+          objects: "always-multiline",
+          imports: "never",
+          exports: "never",
+        },
+      ],
     },
-  },
+  }
 )
