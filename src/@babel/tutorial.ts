0988477d6f9a97326b98e1fe6c7bb97e506ac812
ksheer506 <ksheer506@naver.com>
2024-10-17 13:23:38 +0900

    chore: 플러그인 고려 사항 주석 추가

--- src/@babel/tutorial.ts
+++ src/@babel/tutorial.ts
@@ -15,4 +15,21 @@ const plugin = (babel: Babel): PluginObj => ({
   },
 })
 
+/**
+ * [변수 선언 방법이 다른 경우]
+ * 1. `const { t } = useTranslation()`
+ * 2. `const { t: translate } = useTranslation()`
+ * 3. const i18n = useTranslation()
+ * 
+ * [별도의 커스텀 훅을 사용하는 경우]
+ * 
+ * [<div>의 children으로 사용되는 경우]
+ * 
+ * [<div>의 다른 prop으로 사용되는 경우]
+ * 
+ * [커스텀 컴포넌트의 children로 사용되는 경우]
+ * 
+ * [커스텀 컴포넌트의 다른 prop으로 사용되는 경우] 
+ */
+
 export default plugin
